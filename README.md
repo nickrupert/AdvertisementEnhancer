9c92270594ad0f69539f97a16a90e7d661e46df7
Nick <rupertn@umich.edu>
2018-09-30 21:12:35 -0400

    update readme, comments, and requirements

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # Nick's Ad Enhancer
 
-Flask App
+This is a Python 3 Flask app hosted on an AWS Linux box. (Contact me for the address if you need it!)
 
+This app first checks to see if the ip address is from the US (bonus 1), and proceeds to inject
+up to 5 pieces of data into the original post request before sending it back. Since the publisher_id is required,
+we check that second and abandon the request if we fail to get it (bonus 2). By taking the extra steps of
+integrating this with AWS, we have a substantial number of real-time metrics for the application (bonus 3).
+
+Note: Because it takes MaxMind multiple business days to supply a free account, I used a library
+built around it that doesn't require a license, and so it may be less reliable.
+
+
+### Areas for improvement
+
+1) Asynchronous HTTP requests using AIOHttp, or something similar
+
+2) Caching/memoization of the publisher and demographics information that rarely changes
+
+3) Better testing! These tests cover minimal cases, but more focused tests would be more effective
+
+4) Metrics. I manually tested the latency, but it would be great to have metrics set up to check the amount of time
+it takes things to run (although even free-tier AWS comes with much of that functionality built in).
+
+### Instructions to Run:
+
+1) `python -m venv venv`
+2) `source venv/bin/activate`
+3) `pip install -r requirements.txt`
+4) `python tests.py`
+5) `python app.py`
--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, request
-import requests
-import json
 from geolite2 import geolite2
+import json
+import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def handle_request():
+    """
+    Handle a request and call the various micro-services to update the incoming request
+    """
     data = request.get_json()
 
+    # Check the country of origin and return an error message if the IP isn't from the US
     geo = get_source_country(data['device']['ip'])
     if geo and geo != 'United States':
-        return False
+        error = {'error': 'This service is not available for non-US IP addresses at this time'}
+        return json.dumps(error)
 
     # Do publisher id as early as possible, because if that's missing, we can avoid wasting time on the request
     publisher = get_publisher_info(data['site']['id'])
@@ -23,14 +28,24 @@ def handle_request():
 
     if publisher_id:
         percent_female = get_demographics_info(data['site']['id'])
-        percent_male = 100.0 - percent_female
+        if percent_female:
+            percent_male = 100.0 - percent_female
 
         data = inject_data(data, publisher_id, publisher_name, percent_female, percent_male, geo)
+    else:
+        # If there's no publisher ID, fail and return False
+        return False
 
-    # Post back data
+    # Post back the request after it was injected with additional data
     return json.dumps(data)
 
+
 def get_publisher_info(site_id):
+    """Finds the publisher name and id from the publisher service
+
+        Arguments:
+            site_id -- the id of the site making the request
+        """
     if site_id:
         data = {"q":{"siteID":str(site_id)}}
         r = requests.post('http://159.89.185.155:3000/api/publishers/find', json=data)
@@ -38,26 +53,54 @@ def get_publisher_info(site_id):
     else:
         return None
 
+
 def get_demographics_info(site_id):
+    """ Finds the percent of females visiting a given site
+
+    Arguments:
+        site_id -- the id of the site making the request
+    """
     d = requests.get('http://159.89.185.155:3000/api/sites/{}/demographics'.format(site_id))
     demographics_dict = json.loads(d.text)
     return demographics_dict.get('demographics', {}).get('pct_female', None)
 
+
 def get_source_country(ip):
+    """ Finds the country of origin for the IP address of the incoming request where the site was served
+
+    Arguments:
+        ip -- IP address of the the user who visited the site
+    """
     reader = geolite2.reader()
     return reader.get(ip)['country']['names']['en']
 
+
 def inject_data(data, pub_id, pub_name, female, male, geo):
+    """ Updates the data of the input with the info found from the various micro-services
+
+    Arguments:
+        data -- The input data
+        pub_id -- The publisher's ID
+        pub_name -- The publisher's name
+        female -- The percent of visitors to the site who are female
+        male -- The percent of visitors to the site who are female
+        geo -- The country of origin
+    """
     # Initialize the parts of the dict that weren't present in the input
     data['site']['publisher'] = {}
     data['site']['demographics'] = {}
     data['device']['geo'] = {}
 
-    data['site']['publisher']['id'] = pub_id
-    data['site']['publisher']['name'] = pub_name
-    data['site']['demographics']['female_percent'] = female
-    data['site']['demographics']['male_percent'] = male
-    data['device']['geo']['country'] = geo
+    if pub_id:
+        data['site']['publisher']['id'] = pub_id
+    if pub_name:
+        data['site']['publisher']['name'] = pub_name
+    if female:
+        data['site']['demographics']['female_percent'] = female
+    if male:
+        data['site']['demographics']['male_percent'] = male
+    if geo:
+        data['device']['geo']['country'] = geo
 
     return data
 
